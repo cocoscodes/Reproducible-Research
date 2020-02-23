9a92e8bef79d62d2b91479c4022fa39f791fc5f3
a.solis <solis.acevedo@gmail.com>
2020-02-23 20:58:12 +0100

    latest 200223

---
+++ ReproducibleResearch.R
@@ -0,0 +1,27 @@
+# Elements of reproducinility ----
+# Analytic data
+# Analytic code
+# Documentation
+# Distribution
+
+# Literate Statistical Programming - The text is readable by people and the code is readable by computers
+library(knitr) # is a package that builds on the work of Sweave and provides much more powerful functionality, including the ability to write in Markdown and create a variety of output formats.
+
+# Golden rule -  script everything
+
+# Structure of data analysis ----
+# • Defining the question - defining a question is the most powerful dimension reduction tool you can ever employ.
+# • Defining the ideal dataset
+# • Determining what data you can access
+# • Obtaining the data
+# • Cleaning the data
+# • Exploratory data analysis
+# • Statistical prediction/modeling
+# • Interpretation of results
+# • Challenging of results
+# • Synthesis and write up
+# • Creating reproducible code
+
+# Markdown Syntax - review on report-writing-in-R.pdf
+
+  
