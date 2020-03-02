9d51ec5b7fc854cca6d750b3288287675a03b7f8
a.solis <solis.acevedo@gmail.com>
2020-03-02 22:09:52 +0100

    latest 200302

--- ReproducibleResearch.R
+++ ReproducibleResearch.R
@@ -77,12 +77,63 @@ browseURL("markdown_demo.html") # open in web browser
 install.packages("xtable") # this package will allow better table printing in the final doc
 # Review knitr Rmd doc
 
+# Project Week 2 ----
+getwd()
+setwd("/Users/alejandrosolis/Desktop/Data_Sc/R/Reproducible-Research/RepData_PeerAssessment1")
 
+# Reading file
+data <- read.csv("activity.csv")
+str(data)
+summary(data)
+head(data)
+# Transforming column date to Date type
+data$date <- as.Date(as.character(data$date))
 
+# Histogram of steps per day
+stepsDay <- with(data,tapply(steps,date,sum,na.rm=TRUE))
+hist(stepsDay, main = "Histogram activity",xlab = "Steps per Day", col = "red")
 
+# Mean and Median steps per day
+mean(stepsDay)
+median(stepsDay)
 
+# Time series plot for average steps taken
+meanSteps <- with(data,tapply(steps,interval,mean,na.rm=TRUE))
+plot(meanSteps,main = "Daily average number of steps",type = "l",col="blue",
+     ylab = "Average number of steps",xlab = "5 min interval")
 
+# 5 min interval with the max number of steps
+maxInterval <- data$interval[grep(max(meanSteps),meanSteps)]
 
+# Imputing missing data - by replacing mean values for each 5 min intervals
+library(dplyr)
+meanInt <- function(x){replace(x, is.na(x), mean(x, na.rm = TRUE))}
+newData <- data %>% 
+  group_by(interval) %>%
+  mutate(steps = meanInt(steps))
+head(newData)
+# New Histogram
+stepsDayNew <- with(newData,tapply(steps,date,sum,na.rm=TRUE))
+hist(stepsDayNew, main = "Histogram activity",xlab = "Steps per Day", col = "red")
+# New Mean and Median steps per day
+mean(stepsDayNew)
+median(stepsDayNew)
+# Do these values differ from the estimates from the first part of the assignment? 
+# YES
+# What is the impact of imputing missing data on the estimates of the total daily number of steps?
+# The new histogram shows a normal distribution, new mena and new median have the same value
 
+# Are there differences in activity patterns between weekdays and weekends?
+newData$weekday <- weekdays(data$date)
+newData$weekcat <- ifelse(newData$weekday %in% c("Saturday","Sunday"),"Weekend","Weekday")
 
-
+# Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
+stepsMean <- aggregate(newData$steps,by=list(newData$interval,newData$weekcat),mean)
+colnames(stepsMean) <- c("Interval","Weekcat","Stepsmean")
+str(stepsMean)
+library(ggplot2)
+ggplot(stepsMean,aes(Interval,Stepsmean,color=Weekcat)) +
+  geom_line() +
+  facet_grid(Weekcat~.) + 
+  labs(y="Average number of steps", x="5-minute interval") + 
+  theme(legend.position = "none")
